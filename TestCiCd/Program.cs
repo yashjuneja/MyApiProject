f4ce6b8344beaa5ed8b7bdd7db08eabb759b6ff6
yashul-juneja_qvc <yashul.juneja@qvc.com>
2026-04-13 19:35:17 +0100

    first commit

---
+++ TestCiCd/Program.cs
@@ -0,0 +1,37 @@
+
+namespace TestCiCd
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TestCiCd v1"));
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
